0a73fe914c1292d3e90fe9b8b4701ad5e3c975f7
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-02-16 19:22:56 +0100

    Refactor

--- app.js
+++ app.js
@@ -7,22 +7,10 @@ and thank-you-state div becomes visible */
 
 form.addEventListener("submit", function(i) {
     i.preventDefault(); // Prevent page refresh when submit button is clicked
-    const checkedInput = document.querySelector("input:checked")
-    const selectedRating = document.querySelector(".selection")
-    selectedRating.textContent = `You selected ${checkedInput.getAttribute("value")} out of 5`
-    ratingState.classList.add("hidden")
-    thankYouState.classList.remove("hidden")
+    const checkedInput = document.querySelector("input:checked") // Récupère l'input choisie par l'utilisateur
+    const selectedRating = document.querySelector(".selection") // Récupère le <p> 
+    selectedRating.textContent = `You selected ${checkedInput.getAttribute("value")} out of 5` // Définit le texte affiché dans le <p> en récupérant la valeur de la checkedInput 
+    ratingState.classList.add("hidden") // Cache la div rating-state en lui attribuant la classe hidden (et la propriété display:none associée)
+    thankYouState.classList.remove("hidden") // Affiche la div thank-you-state en lui retirant la classe hidden
 })
 
-/* Add orange background to an input/label when it is clicked by adding "active"
- CSS class to it
-*/
-
-const label = document.querySelectorAll(".rating")
-
-label.forEach(i => {
-    i.addEventListener("click", function () {
-        label.forEach(i => i.classList.remove("active"))
-        this.classList.add("active")
-      })
-})
