d80730cdfd176e2c0e95a20a9a01558c3e664eb0
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-02-17 14:46:21 +0100

    Refactor

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const ratingState = document.querySelector(".rating-state")
 const thankYouState = document.querySelector(".thank-you-state")
 const form = document.querySelector(".form")
+const selectedRating = document.querySelector(".selection") 
 
 /*When an input is checked and submitted, rating-state div is hidden 
 and thank-you-state div becomes visible */
 
 form.addEventListener("submit", function(i) {
     i.preventDefault(); // Prevent page refresh when submit button is clicked
-    const checkedInput = document.querySelector("input:checked") // Récupère l'input choisie par l'utilisateur
-    const selectedRating = document.querySelector(".selection") // Récupère le <p> 
-    selectedRating.textContent = `You selected ${checkedInput.getAttribute("value")} out of 5` // Définit le texte affiché dans le <p> en récupérant la valeur de la checkedInput 
-    ratingState.classList.add("hidden") // Cache la div rating-state en lui attribuant la classe hidden (et la propriété display:none associée)
-    thankYouState.classList.remove("hidden") // Affiche la div thank-you-state en lui retirant la classe hidden
+    const checkedInput = document.querySelector("input:checked")
+    selectedRating.textContent = `You selected ${checkedInput.getAttribute("value")} out of 5` 
+    ratingState.classList.add("hidden") 
+    thankYouState.classList.remove("hidden") 
 })
 
