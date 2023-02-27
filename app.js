779d2f94f4bbf5abc943a988d7d25b40472c33dd
Clément Bartholomé <clementoss@gmail.com>
2023-02-27 09:25:36 +0100

    git commit -m "Refactor"

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
 const ratingState = document.querySelector(".rating-state")
 const thankYouState = document.querySelector(".thank-you-state")
 const form = document.querySelector(".form")
+const selectedRating = document.querySelector(".selection") // Récupère le <p> 
 
 /*When an input is checked and submitted, rating-state div is hidden 
 and thank-you-state div becomes visible */
 
 form.addEventListener("submit", function(i) {
     i.preventDefault(); // Prevent page refresh when submit button is clicked
-    const selector = "input[name=rating]:checked"
-    const checkedInput = document.querySelector(selector)
-        
-    if(checkedInput !== null) {
-        const selectedRating = document.querySelector(".selection")
-        selectedRating.textContent = `You selected ${checkedInput.getAttribute("value")} out of 5`
-        ratingState.classList.add("hidden")
-        thankYouState.classList.remove("hidden")
-    }
-
-})
-
-/* Add orange background to an input/label when it is clicked by adding "active"
- CSS class to it
-*/
-
-const label = document.querySelectorAll(".rating")
-
-label.forEach(i => {
-    i.addEventListener("click", function () {
-        label.forEach(i => i.classList.remove("active"))
-        this.classList.add("active")
-      })
+    const checkedInput = document.querySelector("input:checked")  // Récupère l'input choisie par l'utilisateur
+    selectedRating.textContent = `You selected ${checkedInput.getAttribute("value")} out of 5`  // Définit le texte affiché dans le <p> en récupérant la valeur de la checkedInput 
+    ratingState.classList.add("hidden")  // Cache la div rating-state en lui attribuant la classe hidden (et la propriété display:none associée)
+    thankYouState.classList.remove("hidden")  // Affiche la div thank-you-state en lui retirant la classe hidden
 })
 
